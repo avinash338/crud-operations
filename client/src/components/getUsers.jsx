edd827829e7bb68d09a70d6730c1ed6ff511deb9
Avinash Gajulapalli <avinash.gajulapalli@in.pega.com>
2021-10-31 14:55:19 +0530

    modified code

--- client/src/components/getUsers.jsx
+++ client/src/components/getUsers.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-
 class Users extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            users:[]
-         }
-    }
-    componentDidMount(){
-        this.getusers()
-    }
-    getusers = () => {
-        axios.get("http://localhost:6000/users").then((response) => {
-            console.log(response);
-        }).catch(err=>{
-            console.log(err);
-        }) 
-    }
-    render() { 
-        return ( <div></div> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    this.getusers();
+  }
+  getusers = async () => {
+    await axios
+      .get("http://localhost:3005/users")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    return <div></div>;
+  }
 }
- 
+
 export default Users;
